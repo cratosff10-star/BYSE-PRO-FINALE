5e425a90270cf8a21de646b51386a43f40bac126
Miguel de Macedo Nunes <miguel.macedo@dcx.ufpb.br>
2026-08-21 10:59:40 -0300

    adicionando usuário

--- byse-pro-ts-refactored/server/src/create-user.js
+++ byse-pro-ts-refactored/server/src/create-user.js
@@ -24,4 +24,4 @@ async function createUser(name, email, plainPassword) {
 }
 
 // Executa para criar o seu usuário de teste
-createUser('Usuário CEO', 'matheusbyseceo@gmail.com', 'CEO@b2026');
+createUser('PG SUPLEMENTOS', 'byse.pgsuplementos@gmail.com', 'PG-sup@26');
